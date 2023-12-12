e25ff6b90bf896d866dc5c9db2a6b442d21bdf19
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2023-12-12 09:15:52 -0400

    feat: Lógica para iniciar o jogo

--- GameController.js
+++ GameController.js
@@ -1,7 +1,12 @@
 class GameController {
-    constructor() {
-        this.player = new Player("Jogador");
-        this.match = new Match();
-      }
-    
+  constructor() {
+    this.player = new Player("Jogador");
+    this.match = new Match();
+  }
+
+  iniciarJogo() {
+    console.log("Bem-vindo ao Jogo da Forca!");
+    this.match.iniciarPartida();
+    this.realizarJogada();
+  }
 }
